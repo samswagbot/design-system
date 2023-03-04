52b78d4a031187026b7bc283977dbb7d1e57aec7
Samson <sammossallam@gmail.com>
2023-03-04 18:37:04 -0500

    updating index

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,5 @@
 export { Button } from './components/Button/Button';
 export { Tabs } from './components/Tabs/Tabs';
 export { Modal } from './components/Modal/Modal';
+export { Accordion } from './components/Accordion/Accordion';
+export { Drawer } from './components/Drawer/Drawer';
